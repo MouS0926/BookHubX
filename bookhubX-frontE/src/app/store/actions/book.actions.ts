d566588f14a0ff8d120ae8e7d62d212f4dc34e80
MouS0926 <moupriya2609@gmail.com>
2023-12-03 21:55:31 +0530

    Remove older_folder_fe

--- bookhubX-frontE/src/app/store/actions/book.actions.ts
+++
@@ -1,9 +0,0 @@
-import { createAction, props } from "@ngrx/store";
-import { Book } from "../../components/models/book.model";
-
-
-export const loadBooks=createAction('[Book] Load Books')
-
-export const loadBooksSuccess=createAction('[Book] Load Books Success', props<{books:Book[]}>())
-
-export const loadBooksfailure= createAction('[Book] Load Books Failure', props<{error:string}>())
